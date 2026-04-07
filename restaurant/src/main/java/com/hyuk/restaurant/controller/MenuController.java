905b679d0c2b2925ad21ab9bc13944933f13f5bc
Hyuk <testerhyuk214@gmail.com>
2026-04-07 17:42:28 +0900

    메뉴 가져오기 추가 - Order 서비스에서 메뉴 검증을 위해 메뉴 가져오기 기능 추가

--- restaurant/src/main/java/com/hyuk/restaurant/controller/MenuController.java
+++ restaurant/src/main/java/com/hyuk/restaurant/controller/MenuController.java
@@ -1,16 +1,31 @@
 package com.hyuk.restaurant.controller;
 
+import com.hyuk.restaurant.dto.ResponseMenu;
 import com.hyuk.restaurant.service.BulkMenuInsertService;
+import com.hyuk.restaurant.service.MenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/restaurant-service")
 public class MenuController {
     private final BulkMenuInsertService bulkMenuInsertService;
-    @PostMapping("/restaurant-service/bulk-insert")
+    private final MenuService menuService;
+
+    @PostMapping("/bulk-insert")
     public void bulkInsert() {
         bulkMenuInsertService.processBulkMenuInsert();
+    }
+
+    @GetMapping("/menu/{restaurantId}")
+    public ResponseEntity<List<ResponseMenu>> getMenu(@PathVariable("restaurantId") Long restaurantId) {
+        List<ResponseMenu> response = menuService.getMenu(restaurantId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 }
--- restaurant/src/main/java/com/hyuk/restaurant/repository/MenuRepository.java
+++ restaurant/src/main/java/com/hyuk/restaurant/repository/MenuRepository.java
@@ -3,6 +3,10 @@ package com.hyuk.restaurant.repository;
 import com.hyuk.restaurant.entity.MenuEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MenuRepository extends JpaRepository<MenuEntity, Long> {
     boolean existsByRestaurantId(Long restaurantId);
+
+    List<MenuEntity> findByRestaurantId(Long restaurantId);
 }
